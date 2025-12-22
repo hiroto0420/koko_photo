7bd1a53830550ceac6236ceaeda403bff644d6a5
hiroto <kalsan.fr@gmail.com>
2025-12-22 20:34:57 +0900

    add:投稿作成機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root "posts#index"
-  resources :posts
+  resources :posts, only: %i[new create show index]
 end
